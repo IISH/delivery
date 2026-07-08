0090da5f04c149767a8fe30907cbccf93651c254
Lucien van Wouw <lucien.van.wouw@di.huc.knaw.nl>
2026-07-08 13:46:52 +0200

    Start the cups daemon process before the application.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+set -e
+
+echo "Running CUPS /usr/sbin/cupsd"
+/usr/sbin/cupsd
+sleep 1s
+lpstat -W completed -o
+
 CMD="/opt/java/openjdk/bin/java ${JAVA_OPTS} -jar /app/delivery.jar"
 echo "Running ${CMD}"
 eval "$CMD"
