560a0d78ef353421e1eeeab3f2cadea496355e46
Lucien van Wouw <lucien.van.wouw@di.huc.knaw.nl>
2026-07-16 13:53:35 +0200

    Spelfout in de permissie sequentie tabel. Wordt nu gelijk getrokken aan de database: permissions_id_seq. Hernoem de as naar AS. Eveneens mag /permissions/createform/nummer publiek zijn.

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 tag=$(git describe --tags)
 name="registry.diginfra.net/edepot/delivery"
 
-docker build --tag="${name}:${tag}" .
+docker build --tag="${name}:${tag}" --no-cache .
 docker push "${name}:${tag}"
