fa633475e5236181d9a343c9c8ef8bb716487928
Lucien van Wouw <lucien.van.wouw@di.huc.knaw.nl>
2026-07-20 16:11:02 +0200

    De createform en de mollie callback urls mogen

--- src/main/java/nl/knaw/huc/di/delivery/config/SecurityConfiguration.java
+++ src/main/java/nl/knaw/huc/di/delivery/config/SecurityConfiguration.java
@@ -47,11 +47,11 @@ public class SecurityConfiguration {
                                         "/403",
                                         "/error",
                                         "/record/10622*",
-                                        "/permission/createform/*",
-                                        "/reservation/createform/*",
-                                        "/reproduction/createform/*",
-                                        "/reproduction/confirm/*",
-                                        "/reproduction/order/*",
+                                        "/permission/createform/**",
+                                        "/reservation/createform/**",
+                                        "/reproduction/createform/**",
+                                        "/reproduction/confirm/**",
+                                        "/reproduction/order/**",
                                         "/captcha",
                                         "/").permitAll().
                                 requestMatchers("/actuator/**").hasRole("ACTUATOR"). // internal monitoring management
